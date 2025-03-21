5a6119a016be7350f91553fffac770fb1db4b0e4
adibzr <adibraed1gmail.com>
2025-03-20 22:20:42 -0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,158 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const divs = Array(100).fill(0);
+  const [color, setColor] = useState(
+    Array(100)
+      .fill()
+      .map(() => Array(100).fill(""))
+  );
+
+  const [drawing, setDrawing] = useState(false);
+  const [rightClickCoordenates, setRightClickCoordenates] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [showMenu, setShowMenu] = useState(false);
+  const [chosenColor, setChosenColor] = useState("red");
+  const [menuFadeOut, setMenuFadeOut] = useState(false);
+  const [menuFadeIn, setMenuFadeIn] = useState(false);
+
+  const handleMouseDown = (e) => {
+    if (e.button === 0) {
+      setDrawing(true);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
+  const handleMouseOver = (colIndex, rowIndex) => {
+    if (drawing) {
+      handleColor(colIndex, rowIndex);
+    }
+  };
+  const handleColor = (colIndex, rowIndex) => {
+    const auxColor = [...color];
+    auxColor[colIndex] = [...auxColor[colIndex]];
+    auxColor[colIndex][rowIndex] = auxColor[colIndex][rowIndex]
+      ? ""
+      : chosenColor;
+    setColor(auxColor);
+  };
+
+  const handleClick = (colIndex, rowIndex) => {
+    handleColor(colIndex, rowIndex);
+  };
+
+  const handleChangeColor = (color) => {
+    setMenuFadeOut(true);
+    setChosenColor(color);
+    setTimeout(() => {
+      setShowMenu(false);
+      setMenuFadeOut(false);
+    }, 300);
+  };
+
+  const handleMenuMouseLeave = () => {
+    setMenuFadeOut(true);
+    setTimeout(() => {
+      setShowMenu(false);
+      setMenuFadeOut(false);
+    }, 300);
+  };
+
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      setRightClickCoordenates({ x: e.clientX, y: e.clientY });
+      setMenuFadeIn(true);
+      setTimeout(() => {
+        setShowMenu(true);
+        setMenuFadeIn(false);
+      }, 300);
+      e.preventDefault();
+    };
+    window.addEventListener("contextmenu", handleContextMenu);
+    const calculateGrid = () => {
+      const screenHeight = window.innerHeight;
+      const screenWidth = window.innerWidth;
+      console.log(screenWidth);
+      const cellWidth = Math.round(screenWidth / 100);
+      console.log(cellWidth);
+      const possibleRows = Math.floor(screenHeight / cellWidth);
+
+      setColor(
+        Array(100)
+          .fill()
+          .map(() => Array(possibleRows).fill(""))
+      );
+    };
+
+    calculateGrid();
+
+    window.addEventListener("resize", calculateGrid);
+    return () => {
+      window.removeEventListener("contextmenu", handleContextMenu);
+      window.removeEventListener("resize", calculateGrid);
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div
+        className={`menu ${menuFadeOut ? "fade-out" : ""} ${
+          menuFadeIn ? "fade-in" : ""
+        }`}
+        style={{
+          display: showMenu ? "flex" : "none",
+          top: rightClickCoordenates.y,
+          left: rightClickCoordenates.x,
+        }}
+        onMouseLeave={handleMenuMouseLeave}
+      >
+        <div
+          className="menu-item red"
+          onClick={() => handleChangeColor("red")}
+        ></div>
+        <div
+          className="menu-item blue"
+          onClick={() => handleChangeColor("blue")}
+        ></div>
+        <div
+          className="menu-item yellow"
+          onClick={() => handleChangeColor("yellow")}
+        ></div>
+        <div
+          className="menu-item green"
+          onClick={() => handleChangeColor("green")}
+        ></div>
+        <div
+          className="menu-item orange"
+          onClick={() => handleChangeColor("orange")}
+        ></div>
+      </div>
+
+      <div className="col">
+        {divs.map((_, colIndex) => {
+          return (
+            <div className="row" key={`row-${colIndex}`}>
+              {color[colIndex].map((color, rowIndex) => (
+                <div
+                  key={`cell-${colIndex}-${rowIndex}`}
+                  onClick={() => handleClick(colIndex, rowIndex)}
+                  className={color}
+                  onMouseDown={(e) => handleMouseDown(e)}
+                  onMouseUp={handleMouseUp}
+                  onMouseOver={() => handleMouseOver(colIndex, rowIndex)}
+                ></div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
